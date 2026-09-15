01c3dd4aefdd467252eac750dcb7132eea779a2d
SHRUTI-BARUA <shinibarua7@gmail.com>
2026-09-15 18:22:47 +0530

    Updated : welcome msg

--- src/Chat.jsx
+++ src/Chat.jsx
@@ -25,7 +25,7 @@ function Chat() {
     }, [prevChat, reply]);
     return (
         <>
-            {newChat && <h1>What would you like to discover?</h1>}
+            {newChat && <h1> Raedy for another Session ?</h1>}
             <div className="chats">
                 {
                     prevChat?.map((chat, idx) =>
